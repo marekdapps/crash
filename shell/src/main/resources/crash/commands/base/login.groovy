d7c5bb38bba6e3e67438d9564862f12e60846629
Marek Skocovsky <marek@dapps.cz>
2017-02-22 17:51:32 +0000

    Add Corda logo

--- shell/src/main/resources/crash/commands/base/login.groovy
+++ shell/src/main/resources/crash/commands/base/login.groovy
@@ -5,20 +5,34 @@ welcome = { ->
   } catch (java.net.UnknownHostException ignore) {
     hostName = "localhost";
   }
+  //""";
   return """\
+   ______               __
+  / ____/     _________/ /___ _
+ / /     __  / ___/ __  / __ `/    Shell access by CRaSH ${crash.context.version}
+/ /___  /_/ / /  / /_/ / /_/ /     More on http://www.crashub.org     
+\\____/     /_/   \\__,_/\\__,_/
 
-   _____     ________                 _______    ____ ____
- .'     `.  |        `.             .'       `. |    |    | ${crash.context.version}
-|    |    | |    |    |  .-------.  |    |    | |    |    |
-|    |____| |    `   .' |   _|    |  .    '~_ ` |         |
-|    |    | |    .   `.  .~'      | | `~_    `| |         |
-|    |    | |    |    | |    |    | |    |    | |    |    |
- `._____.'  |____|____| `.________|  `._____.'  |____|____|
-
-Follow and support the project on http://www.crashub.org
-Welcome to $hostName + !
+Welcome to $hostName
 It is ${new Date()} now
+
 """;
+
+//  return """\
+//
+//   _____     ________                 _______    ____ ____
+// .'     `.  |        `.             .'       `. |    |    | ${crash.context.version}
+//|    |    | |    |    |  .-------.  |    |    | |    |    |
+//|    |____| |    `   .' |   _|    |  .    '~_ ` |         |
+//|    |    | |    .   `.  .~'      | | `~_    `| |         |
+//|    |    | |    |    | |    |    | |    |    | |    |    |
+// `._____.'  |____|____| `.________|  `._____.'  |____|____|
+//
+//Follow and support the project on http://www.crashub.org
+//Welcome to $hostName + !
+//It is ${new Date()} now
+//""";
+
 }
 
 prompt = { ->
